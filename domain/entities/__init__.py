60fb699d38977efa2e03f8e4c038b0d527f78afc
Mathieson Sterling <mathieson@seedrocket.io>
2025-04-18 18:43:31 +0200

    fix imports

--- domain/entities/__init__.py
+++ domain/entities/__init__.py
@@ -1,6 +1,6 @@
-from domain.entities.people.borrower import Borrower
 from domain.entities.entity import Entity
 from domain.entities.libraries import Library
 from domain.entities.loan import Loan
 from domain.entities.people import Person, PersonBorrower
+from domain.entities.people.borrower import Borrower
 from domain.entities.thing import Thing
